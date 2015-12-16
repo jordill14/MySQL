8a1d96de0866da0cb3a1f356d5f212c1cb564c9c
jordill14 <jordill14@gmail.com>
2015-12-16 18:00:52 +0100

    v2_insert

--- Mysql/src/Main.java
+++ Mysql/src/Main.java
@@ -3,7 +3,12 @@ public class Main {
 
 	public static void main(String[] args) throws Exception {
 			MySQLAccess dao = new MySQLAccess();
-			dao.readDataBase();
-			dao.insertFila("jordi","jordill14@gmail.com","gourmon.com","gourmet","good food");
+			
+			MethodInsert i = new MethodInsert();
+			
+			//dao.readDataBase();
+			//dao.insertFila("jordi","jordill14@gmail.com","gourmon.com","gourmet","good food");
+			//(int codeing, String nameing, String codemm, double kcal, double carbs, double protein, double fat, double salt, int qty)			
+			i.insertIngredients(10, "olives", "g", 80.5, 67.2, 12.1 , 6.9, 8.1 , 100);
 	}
 }
